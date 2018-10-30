20108bf980bb93e534ebb8a718ea63e5312e5ba9
ar70641 <rayaanselmoa@johndeere.com>
2018-10-29 19:50:44 -0500

    adding brandy's stuff

--- group7_final_project.R
+++ group7_final_project.R
@@ -3,7 +3,10 @@ rm(list = ls())
 
 print("import libraries")
 library(ggplot2)
-library("dplyr")
+library(dplyr)
+library(choroplethr)
+library(choroplethrMaps)
+library(stringr)
 
 print("define global funcitons")
 save_graph <- function(name, plot){
@@ -12,8 +15,6 @@ save_graph <- function(name, plot){
 
 print("define global vars")
 graphList <- list()
-
-
 
 print("fecth dataset")
 url <- 'https://data.iowa.gov/api/views/murf-9x69/rows.csv?accessType=DOWNLOAD'
@@ -28,6 +29,9 @@ df$Serving.Life.with.Possibility.of.Parole <- NULL
 df <- df[complete.cases(df),]
 print("Adding Groupings (Bins)")
 df$Ages_binned <- cut(df$Age, 10 * (0 : 10), labels = c("1-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100"))
+df$Jurisdiction <- tolower(df$Jurisdiction)
+df$Jurisdiction <- na.omit(df$Jurisdiction, na)
+
 # Subsets
 maleCrimes <- df[which(df$gender == 'Male')]
 
@@ -80,6 +84,57 @@ for (i in graphNames) {save_graph(toString(i), graphList[[i]])}
 totalCrimes <- length(df[, "Sex"])
 countofCrimesbyGender <- df %>% group_by(Sex) %>% summarize(count = n())
 myColumns <- names(df) %in% c("Sex", "Offense.Type.Most.Serious.Crime")
+
+
+### group by jurisdiction
+dfj <- group_by(df, Jurisdiction)
+j_summ <- summarize(dfj, value = n())
+head(j_summ)
+
+### subset region by county name
+data(county.regions)
+cr <- county.regions
+cr <- subset(cr, state.abb == "IA")
+cr <- cr[, c("region", "county.name")]
+head(cr)
+
+###reorder summ & cr
+j_summ_ordered <- j_summ[order(tolower(df$Jurisdiction)), ]
+
+cr <- cr[order(cr$county.name), ]
+
+###check if counties and jurisdiction are the same
+identical(tolower(j_summ_ordered), cr$county.name)
+
+fixed <- inner_join(cr, j_summ_ordered, by = c("county.name" = "Jurisdiction"))
+
+identical(tolower(fixed$county.name), cr$county.name)
+
+###copy region info to j_summ
+fixed$region <- cr$region
+
+###get map
+p <- county_choropleth(fixed, title = "Crimes by County", num_colors = 9, state_zoom = "iowa") +
+scale_fill_brewer(name="Frequency", palette="PuRd")
+ggsave(plot = p, filename = "FrequencyofCrimesbyCounty.png", width = 8, height = 4, dpi = 600)
+p
+
+##########################################################
+# Load population data by county
+data(df_pop_county)
+
+# Store the data in a shorter name
+dpc <- df_pop_county
+
+# Make population map
+
+q <- county_choropleth(dpc, title = "Population by County", state_zoom = "iowa") +
+scale_fill_brewer(name = NULL, palette = "Blues")
+q
+ggsave(plot = q, filename = "PopulationbyCounty.png", width = 8, height = 4, dpi = 600)
+
+###########################################################
+
 
 print("processing complete")
 
